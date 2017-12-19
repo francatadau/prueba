ecf2c13983a581b11339819a78e0bb7df3d26546
Francisco Ortola Piles <frorpi@floridauniversitaria.es>
2017-12-19 13:43:58 +0100

    prueba

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba</title>
+    <style media="screen">
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background-color: #111;
+    }
+    </style>
+  </head>
+  <body>
+    <?php $dondeEstamos="About" ?>
+    <ul>
+      <li><a class=<?php if ($dondeEstamos=="home") {
+        echo "active";
+      } ?> href="#home">Home</a> </li>
+      <li><a class=<?php if ($dondeEstamos=="news") {
+        echo "active";
+      } ?> href="#news">News</a> </li>
+      <li><a class=<?php if ($dondeEstamos=="contact") {
+        echo "active";
+      } ?> href="#contact">Contact</a> </li>
+      <li><a class=<?php if ($dondeEstamos=="about") {
+        echo "active";
+      } ?> href="#about">About</a> </li>
+    </ul>
+  </body>
+</html>
---
+++ home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba</title>
+    <style media="screen">
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background-color: #111;
+    }
+    </style>
+  </head>
+  <body>
+    <?php $dondeEstamos="home" ?>
+    <ul>
+      <li><a class=<?php if ($dondeEstamos=="home") {
+        echo "active";
+      } ?> href="#home.php">Home</a> </li>
+      <li><a class=<?php if ($dondeEstamos=="news") {
+        echo "active";
+      } ?> href="#news.php">News</a> </li>
+      <li><a class=<?php if ($dondeEstamos=="contact") {
+        echo "active";
+      } ?> href="#contact.php">Contact</a> </li>
+      <li><a class=<?php if ($dondeEstamos=="about") {
+        echo "active";
+      } ?> href="#about.php">About</a> </li>
+    </ul>
+  </body>
+</html>
